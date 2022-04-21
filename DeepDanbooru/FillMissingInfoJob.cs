648158425517e44afb2ea9fae2f677857108d4d3
da3dsoul <da3dsoul@gmail.com>
2022-04-21 12:34:39 -0400

    Don't Forget Scopes!

--- DeepDanbooru/FillMissingInfoJob.cs
+++ DeepDanbooru/FillMissingInfoJob.cs
@@ -28,21 +28,28 @@ namespace unbooru.DeepDanbooru
 
             try
             {
-                var _context = ServiceProvider.GetRequiredService<IDatabaseContext>();
-                var images = _context.ReadOnlySet<Image>().Where(a => !a.TagSources.Any(b => b.Source == "DeepDanbooruModule"))
-                    .Select(a => a.ImageId).OrderByDescending(a => a).ToArray();
+                int[] images;
+                using (var scope1 = ServiceProvider.CreateScope())
+                {
+                    var dbContext = scope1.ServiceProvider.GetRequiredService<IDatabaseContext>();
+                    images = dbContext.ReadOnlySet<Image>()
+                        .Where(a => !a.TagSources.Any(b => b.Source == "DeepDanbooruModule"))
+                        .Select(a => a.ImageId).OrderByDescending(a => a).ToArray();
+                }
 
-                int i = 0;
                 foreach (var batch in images.Batch(20))
                 {
-                    if (i == 20) Console.WriteLine("Done 20 more");
                     if (context.CancellationToken.IsCancellationRequested) return;
-                    var dbContext = ServiceProvider.GetRequiredService<IDatabaseContext>();
-                    var imageBatch = dbContext.Set<Image>(a => a.Blobs, a => a.TagSources, a => a.Sources)
-                        .Where(a => batch.Contains(a.ImageId)).ToList();
-                    await module.FindTags(ServiceProvider, imageBatch, context.CancellationToken);
-                    dbContext.Save();
-                    i++;
+                    using (var scope2 = ServiceProvider.CreateScope())
+                    {
+                        var dbContext = scope2.ServiceProvider.GetRequiredService<IDatabaseContext>();
+                        var imageBatch = dbContext.Set<Image>(a => a.Blobs, a => a.TagSources, a => a.Sources)
+                            .Where(a => batch.Contains(a.ImageId)).ToList();
+                        await module.FindTags(scope2.ServiceProvider, imageBatch, context.CancellationToken);
+                        dbContext.Save();
+                    }
+
+                    GC.Collect();
                 }
             }
             catch (TaskCanceledException)
